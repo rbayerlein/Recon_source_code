76ac79399c866395471c29d8ee596f13f1cad52d
Reimund <rbayerlein@ucdavis.edu>
2023-08-24 18:18:33 -0700

    Added comments to main function. Added a Documentation file generated with dogygen. The documentation can be created using the command make docs within the app folder. Attention: A folder called Documenation must be created on the same level as the app folder before doing that! Then, to create a pdf file with all documentation run command make inside Documentation/latex. Valid latex installation is required!

--- app/lmrecon.cpp
+++ app/lmrecon.cpp
@@ -1,18 +1,25 @@
 #include <petsys_cscanner.h>
 
 #include <petsys_log.h>
-
+/**
+ * @brief Main entry point for the PET system scanner.
+ *
+ * @param argc Number of command-line arguments.
+ * @param argv Array of command-line arguments.
+ * @return 0 on successful execution.
+ */
 int main(int argc, char* argv[])
 {
-	SystemLog::open("scanner.log");
+	SystemLog::open("scanner.log"); ///< create a log file
 
 	CScanner scanner;
-	scanner.initialize(argv[1]);
+	scanner.initialize(argv[1]); ///< create an initialize an instande of CScanner
 
 	// paralle computing setup
 	// pre initialize number of threads, rbayerlein, 2021-11-15
 	int num_threads = 48;
 #if USE_OMP
+	// Code related to parallelization using OpenMP
 	Projector::NUMBER_OF_THREADS_FP = num_threads;
 	if (argc > 2) {
 		Projector::NUMBER_OF_THREADS_FP = atoi(argv[2]);		
@@ -31,13 +38,14 @@ int main(int argc, char* argv[])
 	//
 
 	// scanner.runRecon();
-	
-	scanner.runRecon2();
+
+	// Running an updated version of the reconstruction algorithm
+	scanner.runRecon2(); ///< Run recon 2. Same as runRecon(), but there is option to invoke KEM reconstruction 
 
 
 	std::fprintf(stderr, "end of recon! Finish\n");
 	
-	SystemLog::close();
+	SystemLog::close(); ///< close log file
 
-	return 0;
+	return 0; ///< return 0. exit program.
 }
